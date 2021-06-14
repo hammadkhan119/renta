c100ecc68e8ad94425abc130a08af15f10348eff
m.khan <m.khan@go-bbg.com>
2021-06-14 18:12:33 +0200

    initial commit

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 const port = 3000;
 app.get('/', (req, res) => {
-  res.send('Lets get started!');
+  res.send('Lets get startedd!');
 });
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
